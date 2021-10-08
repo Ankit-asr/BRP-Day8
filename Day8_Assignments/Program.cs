03f5cee02868ef1d5e9da84a6334b4108edc910c
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-10-08 23:09:01 +0530

    [Ankit]Add : Display day of week when entered Date

---
+++ Day8_Assignments/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day8_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 8 Assignment");
+            DaysOfWeek obj1 = new DaysOfWeek();
+            obj1.Day_Of_Week();
+        }
+    }
+}
